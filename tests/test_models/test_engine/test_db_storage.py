c32970ceb0bd68c73e31bc01a4a7f2410623197e
Enzobat0 <e.batungwan@alustudent.com>
2024-02-09 12:11:53 -0800

    .

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,88 +1,102 @@
 #!/usr/bin/python3
-""" Module for testing db storage"""
-
 import unittest
 import models
-from models.base_model import BaseModel
-from models.engine.db_storage import DBStorage
-from models.state import State
-from models.city import City
 from models.user import User
-from models.amenity import Amenity
 from models.review import Review
+from models.amenity import Amenity
+from models.state import State
 from models.place import Place
+from models.city import City
 import os
 
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                 "Not testing db storage")
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "skip if not fs")
 class TestDBStorage(unittest.TestCase):
-    """ Class to test the DBStorage method """
+    """DB Storage test"""
 
     def setUp(self):
         """ Set up test environment """
-        self.db = models.storage
+        self.storage = models.storage
 
     def tearDown(self):
         """ Remove storage file at end of tests """
-        del self.db
-
-    def test_obj_list_empty(self):
-        """ __objects is initially empty """
-        self.assertEqual(len(self.db.all()), 0)
-
-    def test_new_state(self):
-        """ New State is instantiated with name"""
-        new_state = State("California")
-        self.db.new(new_state)
-        self.db.save()
-        self.assertEqual(len(new_state.id), 36)
-        self.assertEqual(new_state.name, "California")
-
-    def test_new_city(self):
-        """ New City is instantiated with name"""
-        new_state = State("California")
-        new_city = City("San Francisco", state_id=new_state.id)
-        self.db.new(new_state)
-        self.db.save()
-        self.db.new(new_city)
-        self.db.save()
-        self.assertEqual(len(new_city.id), 36)
-        self.assertEqual(new_city.name, "San Francisco")
-        self.assertEqual(new_city.state_id, new_state.id)
+        del self.storage
 
     def test_user(self):
-        """ New User is instantiated with email and password"""
-        new_user = User(email="ssam@test.com",
-                        password="test", first_name="Sam")
-        self.db.new(new_user)
-        self.db.save()
-        self.assertEqual(len(new_user.id), 36)
-        self.assertEqual(new_user.email, "ssam@test.com")
-        self.assertEqual(new_user.password, "test")
-        self.assertEqual(new_user.first_name, "Sam")
+        """ Tests user """
+        user = User(name="John", email="John@gmail.com", password="John12345")
+        user.save()
+        self.assertFalse(user.id in self.storage.all())
+        self.assertEqual(user.name, "John")
+
+    def test_city(self):
+        """ test city """
+        state = State(name="California")
+        state.save()
+        city = City(name="Fresno")
+        city.state_id = state.id
+        city.save()
+        self.assertFalse(city.id in self.storage.all())
+        self.assertEqual(city.name, "Fresno")
+
+    def test_state(self):
+        """ test state"""
+        state = State(name="California")
+        state.save()
+        self.assertFalse(state.id in self.storage.all())
+        self.assertEqual(state.name, "California")
 
     def test_place(self):
-        """ New Place is instantiated with name"""
-        new_place = Place(name="My_little_house", number_rooms=4)
-        self.db.new(new_place)
-        self.db.save()
-        self.assertEqual(len(new_place.id), 36)
-        self.assertEqual(new_place.name, "My_little_house")
-        self.assertEqual(new_place.number_rooms, 4)
+        """Test place"""
+        state = State(name="California")
+        state.save()
+
+        city = City(name="Fresno")
+        city.state_id = state.id
+        city.save()
+
+        user = User(name="John", email="John@gmail.com", password="John12345")
+        user.save()
+
+        place = Place(name="home", number_rooms=4)
+        place.city_id = city.id
+        place.user_id = user.id
+        place.save()
+
+        self.assertFalse(place.id in self.storage.all())
+        self.assertEqual(place.number_rooms, 4)
+        self.assertEqual(place.name, "home")
 
     def test_amenity(self):
-        """ New Amenity is instantiated with name"""
-        new_amenity = Amenity(name="Microwave")
-        self.db.new(new_amenity)
-        self.db.save()
-        self.assertEqual(len(new_amenity.id), 36)
-        self.assertEqual(new_amenity.name, "Microwave")
+        """ test amenity """
+        amenity = Amenity(id="test1_id", name="Starlink")
+        amenity.save()
+        self.assertFalse(amenity.name, None)
+        self.assertTrue(amenity.name, "Starlink")
 
     def test_review(self):
-        """ New Review is instantiated with text"""
-        new_review = Review(text="Warm bath and quality amenities")
-        self.db.new(new_review)
-        self.db.save()
-        self.assertEqual(len(new_review.id), 36)
-        self.assertEqual(new_review.text, "Warm bath and quality amenities")
+        """ test review """
+        state = State(name="California")
+        state.save()
+
+        city = City(name="Fresno")
+        city.state_id = state.id
+        city.save()
+
+        user = User(name="John", email="John@gmail.com", password="John12345")
+        user.save()
+
+        place = Place(name="home", number_rooms=4)
+        place.city_id = city.id
+        place.user_id = user.id
+        place.save()
+
+        review = Review(text="no comment", place_id=place.id, user_id=user.id)
+        review.save()
+
+        self.assertFalse(review.id in self.storage.all())
+        self.assertEqual(review.text, "no comment")
+
+
+if __name__ == '__main__':
+    unittest.main()
