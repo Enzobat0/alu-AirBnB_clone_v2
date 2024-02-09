b1e9cd33875ee99acadbaa6838e53d52bc31b70c
Enzobat0 <e.batungwan@alustudent.com>
2024-02-09 12:00:01 -0800

    .

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,56 +7,40 @@ from uuid import UUID
 import json
 import os
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
-                 'basemodel test not supported')
+
 class test_basemodel(unittest.TestCase):
-    """ __summary__
-    The test class for base_model
-    """
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """ __summary__
-        The init class for creating a test class instance. 
-        """
+        """ """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ __summary__
-        The setup method
-        """
+        """ """
         pass
 
     def tearDown(self):
-        """_summary_
-        The teardown methid
-        """
         try:
             os.remove('file.json')
         except:
             pass
 
     def test_default(self):
-        """__summary__
-        The test of default methods
-        """
+        """ """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ __summary__
-        Test for Kwargs
-        """
+        """ """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ __summary__
-        Testing int kwargs
-        """
+        """ """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -64,9 +48,7 @@ class test_basemodel(unittest.TestCase):
             new = BaseModel(**copy)
 
     def test_save(self):
-        """ __summary__
-        Testing save 
-        """
+        """ Testing save """
         i = self.value()
         i.save()
         key = self.name + "." + i.id
@@ -75,48 +57,41 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ __summary__
-        """
+        """ """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ __summary__
-        """
+        """ """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ __summary__
-        """
+        """ """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ __summary__
-        """
+        """ """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ __summary__
-        """
+        """ """
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ __summary__
-        """
+        """ """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ __summary__
-        """
+        """ """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
